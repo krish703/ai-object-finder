8ea60c04d1f71996d84227c7bf31613ccb81d68c
krish703 <78411788+krish703@users.noreply.github.com>
2021-12-31 16:49:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload(){
+ 
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center(); 
+}
+
+function draw(){
+    image(video,0,0,480,380);
+  
+}
+
+
+function gotResult(error, results){
+
+}
+
+function start(){
+    
+}
+
+function modelLoaded(){
+}
